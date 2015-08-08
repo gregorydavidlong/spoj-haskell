167df26a2ac96bc2754889088cf43e55783578a6
Thankyou Forbeingafriend <thankyouforbeingafriend@gmail.com>
2015-08-08 12:12:18 -0700

    finished PRIME1

--- PRIME1/src/Lib.hs
+++ PRIME1/src/Lib.hs
@@ -1,6 +1,24 @@
 module Lib
-    ( someFunc
+    ( parseNumLines,
+      getRange,
+      getPrimesInRange,
+      isPrime
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+parseNumLines :: String -> Int
+parseNumLines numLinesStr = read numLinesStr
+
+getRange :: String -> (Int, Int)
+getRange input = (x, y) where
+	(x:y:_) = map read (words input)
+
+getPrimesInRange :: (Int, Int) -> [Int]
+getPrimesInRange (x,y) = filter isPrime [x..y]
+
+numbersToCheckAgainst :: Int -> [Int]
+numbersToCheckAgainst x = [2 .. (ceiling (sqrt (fromIntegral x)))]
+
+isPrime :: Int -> Bool
+isPrime 1 = False
+isPrime 2 = True
+isPrime x = not (any (\y -> (rem x y) == 0) (numbersToCheckAgainst x))
--- PRIME1/test/Spec.hs
+++ PRIME1/test/Spec.hs
@@ -1,2 +1,32 @@
+import Test.Hspec
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "getRange" $ do
+    it "parses a string into an array on integers" $ do
+      getRange "1 2" `shouldBe` (1, 2)
+
+  describe "isPrime" $ do
+    it "check 1" $ do
+      isPrime 1 `shouldBe` False
+    it "check 2" $ do
+      isPrime 2 `shouldBe` True
+    it "check 3" $ do
+      isPrime 3 `shouldBe` True
+    it "check 4" $ do
+      isPrime 4 `shouldBe` False
+    it "check 1001" $ do
+      isPrime 1001 `shouldBe` False
+    it "check 10001" $ do
+      isPrime 10001 `shouldBe` False
+    it "check 100001" $ do
+      isPrime 100001 `shouldBe` False
+    it "check 100003" $ do
+      isPrime 100003 `shouldBe` True
+    it "check 1000003" $ do
+      isPrime 1000003 `shouldBe` True
+    it "check 1000000003" $ do
+      isPrime 1000000003 `shouldBe` False
+    it "check 1000000007" $ do
+      isPrime 1000000007 `shouldBe` True
